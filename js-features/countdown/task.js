ca537bcc2ec8b1b0b8832f41eeddb2b4946b632b
Stanislav <stas_sokolov_2001@mail.ru>
2022-09-01 14:18:52 +0600

    hw

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,6 +1,23 @@
-const timer = function () {
-	const output = document.getElementById('timer')
-	output.textContent -= 1
-}
-setInterval(timer, 1000);
-alert('Вы победили в конкурсе!')
+// Task 1.1
+
+const output = document.getElementById('timer');
+const timer = setInterval(function () {
+	output.textContent -= 1;
+	if (output.textContent == 0) {
+		clearInterval();
+		alert('Вы победили в конкурсе!');
+	}
+}, 1000);
+
+// Task 1.2
+
+
+// Task 1.3
+const updateTimer = setInterval(function () {
+	output.textContent -= 1;
+	if (output.textContent == 0) {
+		clearInterval()
+		window.location.href = 'https://www.google.ru/'
+	}
+
+}, 1000)
