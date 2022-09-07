1ab538b0616dcff4ebfd7a4df186aafe851e16f0
Stanislav <stas_sokolov_2001@mail.ru>
2022-09-07 19:40:28 +0600

    dz

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,14 +1,9 @@
-const buttonClosedPop = Array.from(document.querySelectorAll('.modal__close_times'));
-const beforForm = document.querySelector('#modal_main');
-const button = document.querySelector('.btn');
-const afterForm = document.querySelector('#modal_success');
-
-button.addEventListener('click', function() {
-    afterForm.classList.add('modal_active');
-    beforForm.classList.remove('modal_active');
-});
-
-buttonClosedPop.forEach(item => item.addEventListener('click', function() {
-    beforForm.classList.remove('modal_active');
-    afterForm.classList.remove('modal_active');
-}));
+let menuLink = Array.from(document.querySelectorAll('.menu__link'));
+menuLink.forEach(item => item.addEventListener('click', function (event) {
+        if ((item.parentElement.querySelectorAll('.menu_sub').length === 1)) {
+            event.preventDefault();
+            let menuSub = Array.from(item.parentElement.querySelectorAll('.menu_sub'));
+            menuSub.forEach(sub => sub.classList.toggle('menu_active'));
+        }
+    }
+));
